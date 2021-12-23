70555fd683c7375883306842826bd24059e3e8bb
Andre Torres <andre855@gmail.com>
2021-12-23 05:37:06 +0100

    Refactor for day 5

--- src/day5/part2.ts
+++ src/day5/part2.ts
@@ -1,20 +1,8 @@
-import { parseInput, expandLine  } from "./utils";
+import { parseInput, expandLine, countOverlap as countOverlappingPositions  } from "./utils";
 
 export function countOverlap(input: string): number {
   const positions = parseInput(input)
     .flatMap(line => expandLine(line));
 
-  const positionCounter = new Map<string, number>();
-
-  for (const position of positions) {
-    const pos = `${position.x}-${position.y}`;
-    positionCounter.set(pos, (positionCounter.get(pos) ?? 0) + 1);
-  }
-
-  let result = 0;
-  for (const value of positionCounter.values()) {
-    if (value > 1) result++;
-  }
-
-  return result;
+  return countOverlappingPositions(positions);
 }
--- src/day5/utils.ts
+++ src/day5/utils.ts
@@ -59,3 +59,19 @@ function expandLineHorizontal(line: Line): Position[] {
 
   return result;
 }
+
+export function countOverlap(positions: Position[]): number {
+  const positionCounter = new Map<string, number>();
+
+  for (const position of positions) {
+    const pos = `${position.x}-${position.y}`;
+    positionCounter.set(pos, (positionCounter.get(pos) ?? 0) + 1);
+  }
+
+  let result = 0;
+  for (const value of positionCounter.values()) {
+    if (value > 1) result++;
+  }
+
+  return result;
+}
